2a143e408af5e1a585398465e6875530d445eba6
Karthigha <Karthies12@gmail.com>
2021-10-05 17:02:46 +0100

    Big.java is added

---
+++ Big.java
@@ -0,0 +1,19 @@
+
+public class Big {
+
+	public static void main(String[] args)
+	{
+	int a,b;
+	a=5;
+	b=6;
+	if(a>b)
+	{
+		System.out.println("A is big");		
+	}
+	else 
+	{
+		System.out.println("B is big");
+	}
+	}
+
+}
